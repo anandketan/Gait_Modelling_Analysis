8b4bb4a9aa831af9ddb2109e896dd2f10ac565bc
Nikhil George <nikhil.george98@gmail.com>
2021-09-14 18:46:32 +0530

    Updated threaded collection and logging script

--- threaded_data_collection_combined.py
+++ threaded_data_collection_combined.py
@@ -6,8 +6,9 @@ import datetime
 from datetime import datetime
 import signal
 import os
+import keyboard
 
-
+file_name_flag_combined = 'combined_data_flagged.csv'
 file_name_combined = 'combined_data.csv'
 script_dir = os.path.dirname(os.path.abspath(__file__))
 dest_dir = os.path.join(script_dir, 'DataFolder', '{}_{}_{}_{}'.format(datetime.now().date(), datetime.now().time().hour, datetime.now().time().minute, datetime.now().time().second))
@@ -15,6 +16,7 @@ try:
     os.makedirs(dest_dir)
 except OSError:
     pass # already exists
+path_flag_combined = os.path.join(dest_dir, file_name_flag_combined)
 path_combined = os.path.join(dest_dir, file_name_combined)
 
 
@@ -22,8 +24,6 @@ try:
     collect_time = int(input("Enter time in seconds to collect data\n"))
 except:
     collect_time = 60
-
-
 
 
 port1 = 9999
@@ -54,10 +54,16 @@ port7 = 3333
 filename7 = input('Enter filename for sensor(N) connected on port {}\n'.format(port7))
 path7 = os.path.join(dest_dir, filename7+'_sensorN.csv')
 
-with open(path_combined, 'w') as comb:
-    comb.write('yaw9, pitch9, roll9,yaw8, pitch8, roll8,yaw7, pitch7, roll7,yaw6, pitch6, roll6,yaw5, pitch5, roll5,yaw4, pitch4, roll4,yaw3, pitch3, roll3, Thread, port, time\n')
+flags = {9999:True, 8888:True, 7777:True, 6666:True, 5555:True, 4444:True, 3333:True}
+
+with open(path_combined, 'w') as comb, open(path_flag_combined, 'w') as comb_flag:
+    comb.write('update#,yaw9, pitch9, roll9,yaw8, pitch8, roll8,yaw7, pitch7, roll7,yaw6, pitch6, roll6,yaw5, pitch5, roll5,yaw4, pitch4, roll4,yaw3, pitch3, roll3, ,Thread, port, time,rate, heel_strike\n')
+    comb_flag.write('update#,yaw9, pitch9, roll9,yaw8, pitch8, roll8,yaw7, pitch7, roll7,yaw6, pitch6, roll6,yaw5, pitch5, roll5,yaw4, pitch4, roll4,yaw3, pitch3, roll3, , Thread, port, time,rate, heel_strike\n')
 
 all_data = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+sample_count = 0
+flag_sample_count = 0
+hs = 0
 
 class camThread(threading.Thread):
     def __init__(self, port, filename):
@@ -76,12 +82,19 @@ class camThread(threading.Thread):
 
 
 def data_receive(threadname, filename, sock, lock, exit_event, port):
+    global all_data
+    global sample_count
+    global flag_sample_count
+    global hs
+
+    global flags
+
     lock.acquire()
     count = 0
     rate = 0
     initial = 0
-    with open(filename, 'w') as fp, open(path_combined, 'a') as comb:
-        fp.write('AccX,AccY,AccZ,GyroX,GyroY,GyroZ,Q1, Q2, Q3, Q4, Yaw,Pitch,Roll,count,imu_time,receiver_time,DATA_RATE\n')
+    with open(filename, 'w') as fp, open(path_combined, 'a') as comb, open(path_flag_combined, 'w') as comb_flag:
+        fp.write('AccX,AccY,AccZ,GyroX,GyroY,GyroZ,Q1, Q2, Q3, Q4, Yaw,Pitch,Roll,count,imu_time,receiver_time,DATA_RATE,heelstrike\n')
         # fp.write('count, time, rate\n')
         # print("{} has entered here at {}".format(threadname, datetime.now().time()))
         start = time.time()
@@ -89,19 +102,29 @@ def data_receive(threadname, filename, sock, lock, exit_event, port):
             # print("{} has entered here at {}".format(threadname, datetime.now().time()))    
             while time.time()-start<collect_time:
                 # print("{} has entered here at {}".format(str(threadname).split(',')[0].split('(')[1], datetime.now().time()))
+                flags[port]=True
                 try:
                     data = sock.recv(1024).decode("utf-8")
                 except socket.error:
                     continue
+                
+                print(str(threadname).split(',')[0].split('(')[1], port)
+                sample_count+=1
                 all_data_str = ""
+                
                 if (count%100)==0:
                     cur_time=time.time()
                 if (count%100)==99:
-                    rate=99/(time.time()-cur_time)
-                fp.write(str(data)+','+ str(time.time())+','+ str(rate)+'\n')
-
-                # fp.write(str(count)+','+ str(datetime.now().time())+','+ str(rate)+'\n')
+                    rate=100/(time.time()-cur_time)
+                
+                if (keyboard.is_pressed("q")):
+                    hs =100
+                else:
+                    hs=0
+                
+                fp.write(str(data)+','+ str(time.time())+','+ str(rate)+','+ str(hs)+'\n')
                 count+=1
+
                 d= str(data).split(',')
                 if port==9999:
                     all_data[0:3] = d[-5:-2]
@@ -120,13 +143,30 @@ def data_receive(threadname, filename, sock, lock, exit_event, port):
                 for i in all_data:
                     all_data_str = all_data_str + str(i) + ','
                 all_data_str.strip(',')
-                comb.write(str(all_data_str) + ',' +str(threadname).split(',')[0].split('(')[1] + ',' + str(port) + ',' + str(time.time()))
+                
+                if (sample_count%100)==0:
+                    cur_comb_time=time.time()
+                if (sample_count%100)==99:
+                    comb_rate=100/(time.time()-cur_comb_time)
+                comb.write(str(sample_count)+','+str(all_data_str) + ',' +str(threadname).split(',')[0].split('(')[1] + ',' + str(port) + ',' + str(time.time())+ ',' + str(comb_rate)+','+str(hs)+'\n')
+                
+                if all(value == True for value in flags.values()):
+                    if (flag_sample_count%100)==0:
+                        cur_comb_flag_time=time.time()
+                    if (flag_sample_count%100)==99:
+                        comb_flag_rate=100/(time.time()-cur_comb_flag_time)
+                    flag_sample_count+=1
+                    comb_flag.write(str(flag_sample_count)+','+str(all_data_str) + ',' +str(threadname).split(',')[0].split('(')[1] + ',' + str(port) + ',' + str(time.time())+ ',' + str(comb_flag_rate)+','+str(hs)+'\n')
+                    for key, value in flags.items():
+                        flags[key]=False
+
                 if count == 1:
-                    print(count)
+                    # print(count)
                     initial = int(d[-2]) - count - 1
                 # print(initial, int(d[-2]))
                 print(d[-5:-1], " Received:", count-1, " Data rate:",rate," Drop:",int(d[-2])-count-1-initial, " Port:", port)
                 print("count:", count, "Rate:", rate)
+                
                 if exit_event.is_set():
                     break
         except Exception as e: 

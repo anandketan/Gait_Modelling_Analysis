a8acca8912fd746a051080606b1356e7494db445
raafay-cit <syedraafay@gmail.com>
2022-05-12 13:06:03 +0530

    Added universal receive test script

--- recvTest.py
+++ recvTest.py
@@ -1,9 +1,10 @@
 import socket
 import time
 
-def listenfordata(sensor, location):
+
+def listenfordata(sensor):
     try:
-        data[sensor] = sockets[location].recv(1024).decode("utf-8")
+        data[sensor] = socks[sensor].recv(1024).decode("utf-8")
         if count[sensor] == 0:
             initialtime[sensor] = time.time()
         count[sensor] += 1
@@ -11,14 +12,14 @@ def listenfordata(sensor, location):
             rate[sensor] = round(count[sensor] / (time.time() - initialtime[sensor]), 2)
             count[sensor] = 0
     except socket.error:
-        pass
+        if time.time() - initialtime[sensor] > 10:
+            rate[sensor] = 0
+            count[sensor] = 0
 
 
-# device_list = ['D', 'C', 'B', 'F', 'H', 'G', 'N']
-# port_list = [8888, 7777, 6666, 9000, 8000, 4444, 3333]
-
-device_list = ['E', 'D', 'C', 'B', 'A', 'G', 'N', 'F']
-port_list = [9999, 8888, 7777, 6666, 5555, 4444, 3333, 9000]
+noOfDevices = 9
+device_list = ['E', 'D', 'C', 'B', 'A', 'G', 'N', 'F', 'H']
+port_list = [9999, 8888, 7777, 6666, 5555, 4444, 3333, 9000, 8000]
 
 count = {}
 initialtime = {}
@@ -27,16 +28,14 @@ data = {}
 
 for device in device_list:
     count[device] = 0
-    initialtime[device] = 0
-    rate[device] = 0
-    if device!='E':
+    initialtime[device] = 0.0
+    rate[device] = 0.0
+    if device != 'E':
         data[device] = "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0"
     else:
         data[device] = "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0"
 
-print(data)
-
-sock_list = [socket.socket(socket.AF_INET, socket.SOCK_DGRAM) for _ in range(5)]
+sock_list = [socket.socket(socket.AF_INET, socket.SOCK_DGRAM) for _ in range(noOfDevices)]
 for port, sock in zip(port_list, sock_list):
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     sock.bind(("0.0.0.0", port))
@@ -46,15 +45,8 @@ socks = {}
 for device, sock in zip(device_list, sock_list):
     socks[device] = sock
 
-# sockets = {'right_bicep': socks['D'], 'right_thigh': socks['C'], 'right_shank': socks['B'], 'back': socks['F'],
-#            'right_forearm': socks['H']}
-
-sockets = {'button': socks['E'], 'right_shank': socks['D'], 'right_thigh': socks['C'], 'left_thigh': socks['B'],
-           'ultrasonic': socks['A'], 'right_foot': socks['G'], 'left_shank': socks['N'], 'left_foot': socks['F']}
-
-
 while True:
-    for sensor, location in zip(device_list, sockets):
-        listenfordata(sensor, location)
+    for sensor in device_list:
+        listenfordata(sensor)
     print({x: rate[x] for x in rate if rate[x] > 0})
-    print(rate)
+    # print(rate)

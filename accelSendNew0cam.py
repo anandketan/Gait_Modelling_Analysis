d018e03f99697412371ae6854efe7cd1bd40ee52
raafay-cit <syedraafay@gmail.com>
2022-07-08 13:44:25 +0530

    Added working game input file

--- accelSendNew0cam.py
+++ accelSendNew0cam.py
@@ -1,4 +1,4 @@
-# Game input(copy from laptop)
+# Game input
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,8 +8,8 @@ import math
 import os
 import datetime
 from datetime import datetime
-import pyrealsense2 as rs
-import cv2
+# import pyrealsense2 as rs
+# import cv2
 
 import keyboard
 import utils_sensor_data as utils
@@ -53,7 +53,7 @@ calcRoll = 12
 Yaw = 13
 Pitch = 14
 Roll = 15
-count_sensor = 16
+packetCount = 16
 timer = 17
 hs = 18
 dist = 19
@@ -65,11 +65,19 @@ counter = 0
 k = 0
 looprate = 0
 
+noOfSensors = 4
+
+right_thigh, rtport = 'F', 9000
+right_shank, rsport = 'B', 6666
+back, backport = 'D', 8888
+right_bicep, rbport = 'H', 8000
+# right_forearm, rfport = 'F', 9000
+
 writes = 0
 writeCounter = 0
 writeRate = 0
 
-name = 'Test'
+name = 'teset'
 trial = input("Trial number?\n")
 file_name_all = '{}_{}_allGameSensorData_{}_{}_{}_{}.csv'.format(name, trial, datetime.now().date(),
                                                                  datetime.now().time().hour,
@@ -85,7 +93,7 @@ except OSError:
 path_game = os.path.join(dest_dir, file_name_all)
 path_pose = os.path.join(dest_dir, file_name_pose)
 
-# UDP_IP = "192.168.100.1"
+#UDP_IP = "192.168.100.1"
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5065
 sockUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -96,8 +104,10 @@ reach = 0
 forward = 0
 side = 0
 
-device_list = ['B', 'C', 'F', 'D', 'H']
-port_list = [6666, 7777, 9000, 8888, 8000]
+joint_device = {'right_thigh': right_thigh, 'right_shank': right_shank, 'back': back, 'right_bicep': right_bicep}
+
+device_list = [right_thigh, right_shank, back, right_bicep]
+port_list = [rtport, rsport, backport, rbport]
 
 prevdata = {}
 data = {}
@@ -136,7 +146,7 @@ for device in device_list:
     data[device] = "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0"
     prevdata[device] = "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0"
 
-sock_list = [socket.socket(socket.AF_INET, socket.SOCK_DGRAM) for _ in range(5)]
+sock_list = [socket.socket(socket.AF_INET, socket.SOCK_DGRAM) for _ in range(noOfSensors)]
 for port, sock in zip(port_list, sock_list):
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     sock.bind(("0.0.0.0", port))
@@ -146,13 +156,16 @@ socks = {}
 for device, sock in zip(device_list, sock_list):
     socks[device] = sock
 
-sockets = {'right_bicep': socks['B'], 'right_thigh': socks['C'], 'right_shank': socks['F'], 'back': socks['D'],
-           'right_forearm': socks['H']}
+sockets = {'right_thigh': socks[right_thigh], 'right_shank': socks[right_shank], 'back': socks[back],
+           'right_bicep': socks[right_bicep]}
 
 with open(os.path.join(dest_dir, 'device_list.txt'), 'w') as f:
     f.write(str(device_list) + '\n')
     f.write(str(port_list) + '\n')
-    f.write(str(sockets) + '\n')
+    # f.write(str(sockets) + '\n')
+    f.write(str(joint_device) + '\n')
+    f.write('Storage order: {}{}{}{}\n'.format(right_thigh, right_shank, back, right_bicep))
+    f.write('File col order: CBGN\n')
 
 with open(path_pose, 'w') as file1, open(path_game, 'w') as file2:
     file1.write('count,'
@@ -163,7 +176,6 @@ with open(path_pose, 'w') as file1, open(path_game, 'w') as file2:
                 'BackGravAccX,BackGravAccY,BackGravAccZ,Back_roll,Back_pitch,Back_yaw,Forward,Sway'
                 '\n')
     file2.write('count,'
-                'FlagD,AccX_D,AccY_D,AccZ_D,GyroX_D,GyroY_D,GyroZ_D,_DQ1,_DQ2,_DQ3,_DQ4,_DYawQ,_DPitchQ,_DRollQ,_DYaw,_DPitch,_DRoll,_Dcount,_Dtime,_DHS,_DDist,_DgravaccX,_DgravaccY,_DgravaccZ,_DSendRate,_DRecvRate,'
                 'FlagC,AccX_C,AccY_C,AccZ_C,GyroX_C,GyroY_C,GyroZ_C,_CQ1,_CQ2,_CQ3,_CQ4,_CYawQ,_CPitchQ,_CRollQ,_CYaw,_CPitch,_CRoll,_Ccount,_Ctime,_CHS,_CDist,_CgravaccX,_CgravaccY,_CgravaccZ,_CSendRate,_CRecvRate,'
                 'FlagB,AccX_B,AccY_B,AccZ_B,GyroX_B,GyroY_B,GyroZ_B,_BQ1,_BQ2,_BQ3,_BQ4,_BYawQ,_BPitchQ,_BRollQ,_BYaw,_BPitch,_BRoll,_Bcount,_Btime,_BHS,_BDist,_BgravaccX,_BgravaccY,_BgravaccZ,_BSendRate,_BRecvRate,'
                 'FlagG,AccX_G,AccY_G,AccZ_G,GyroX_G,GyroY_G,GyroZ_G,_GQ1,_GQ2,_GQ3,_GQ4,_GYawQ,_GPitchQ,_GRollQ,_GYaw,_GPitch,_GRoll,_Gcount,_Gtime,_GHS,_GDist,_GgravaccX,_GgravaccY,_GgravaccZ,_GSendRate,_GRecvRate,'
@@ -186,40 +198,40 @@ with open(path_pose, 'w') as file1, open(path_game, 'w') as file2:
         for sensor in device_list:
             prevyaw[sensor], d[sensor][calcYaw], nyaw[sensor] = utils.correctYaw(prevyaw[sensor], d[sensor][calcYaw], nyaw[sensor])
             prevroll[sensor], d[sensor][calcRoll], nroll[sensor] = utils.correctRoll(prevroll[sensor], d[sensor][calcRoll], nroll[sensor])
-            prevpitch[sensor], d[sensor][calcPitch], npitch[sensor] = utils.correctPitch(prevpitch[sensor], d[sensor][calcPitch], npitch[sensor])
+            prevpitch[sensor], d[sensor][calcPitch], npitch[sensor] = float(prevpitch[sensor]), float(d[sensor][calcPitch]), npitch[sensor]
+            # prevpitch[sensor], d[sensor][calcPitch], npitch[sensor] = utils.correctPitch(prevpitch[sensor], d[sensor][calcPitch], npitch[sensor])
 
-        pose_detect = str(d['D'][gravaccx]) + "," + str(d['D'][gravaccy]) + "," + str(d['D'][gravaccz]) + "," + \
-                      str(d['B'][gravaccx]) + "," + str(d['B'][gravaccy]) + "," + str(d['B'][gravaccz]) + "," + \
-                      str(d['H'][gravaccx]) + "," + str(d['H'][gravaccy]) + "," + str(d['H'][gravaccz]) + "," + \
-                      str(d['C'][gravaccx]) + "," + str(d['C'][gravaccy]) + "," + str(d['C'][gravaccz]) + "," + \
-                      str(d['F'][gravaccx]) + "," + str(d['F'][gravaccy]) + "," + str(d['F'][gravaccz])
+        pose_detect = str(d[back][gravaccx]) + "," + str(d[back][gravaccy]) + "," + str(d[back][gravaccz]) + "," + \
+                      str(0) + "," + str(0) + "," + str(0) + "," + \
+                      str(d[right_bicep][gravaccx]) + "," + str(d[right_bicep][gravaccy]) + "," + str(d[right_bicep][gravaccz]) + "," + \
+                      str(d[right_thigh][gravaccx]) + "," + str(d[right_thigh][gravaccy]) + "," + str(d[right_thigh][gravaccz]) + "," + \
+                      str(d[right_shank][gravaccx]) + "," + str(d[right_shank][gravaccy]) + "," + str(d[right_shank][gravaccz])
 
         sockUDP.sendto(pose_detect.encode(), (UDP_IP, UDP_PORT))
         print(pose_detect)
 
-        raRoll = d['B'][calcRoll] - d['H'][calcRoll]
-        raYaw = d['B'][calcYaw] - d['H'][calcYaw]
-        raPitch = d['B'][calcPitch] - d['H'][calcPitch]
-        rlRoll = d['C'][calcRoll] - d['F'][calcRoll]
-        rlYaw = d['C'][calcYaw] - d['F'][calcYaw]
-        rlPitch = d['C'][calcPitch] - d['F'][calcPitch]
+        raRoll = d[right_bicep][calcRoll]
+        raYaw = d[right_bicep][calcYaw]
+        raPitch = d[right_bicep][calcPitch]
+        rlRoll = d[right_thigh][calcRoll] - d[right_shank][calcRoll]
+        rlYaw = d[right_thigh][calcYaw] - d[right_shank][calcYaw]
+        rlPitch = d[right_thigh][calcPitch] - d[right_shank][calcPitch]
 
-        if float(d['B'][gravaccx]) > 2.5 and float(d['H'][gravaccx]) > 2.5 and float(d['B'][gravaccy]) < 0:
-            reach = 1
-        else:
-            reach = 0
+        # if float(d[right_bicep][gravaccx]) > 2.5:
+        #     reach = 1
+        # else:
+        #     reach = 0
 
-        if ((float(d['C'][gravaccz]) < 0 and float(d['F'][gravaccz]) < 0) or float(d['C'][gravaccz]) < -2 or float(
-                d['F'][gravaccz]) < -2) and (float(d['C'][gravaccy]) > -2.5 and float(d['C'][gravaccy]) < 2.5) and (
-                float(d['F'][gravaccy]) > -2.5 and float(d['F'][gravaccy]) < 2.5) and float(
-                d['C'][gravaccx]) < 0 and float(d['C'][gravaccx]) < 0:
-            step = 1
-            # print("Step")
-        else:
-            step = 0
-            # print("No Step")
+        # if ((float(d[right_thigh][gravaccz]) < 0 and float(d[right_shank][gravaccz]) < 0) or float(d[right_thigh][gravaccz]) < -2 or float(
+        #         d[right_shank][gravaccz]) < -2) and (-2.5 < float(d[right_thigh][gravaccy]) < 2.5) and (
+        #         -2.5 < float(d[right_shank][gravaccy]) < 2.5) and float(d[right_thigh][gravaccx]) < 0 and float(d[right_thigh][gravaccx]) < 0:
+        #     step = 1
+        #     # print("Step")
+        # else:
+        #     step = 0
+        #     # print("No Step")
 
-        if flags['F'] or flags['C'] or flags['B'] or flags['D'] or flags['H']:
+        if flags[right_shank] or flags[right_thigh] or flags[back] or flags[right_bicep]:
             writes += 1
             if writeCounter == 0:
                 write_cur_time = time.time()
@@ -231,20 +243,19 @@ with open(path_pose, 'w') as file1, open(path_game, 'w') as file2:
             timeWrite = time.time() - init
 
             file2.write(str(writes) + ',' +
-                        str(flags['B']) + ',' + str(data['B']) + ',' + str(sendrate['B']) + ',' + str(rate['B']) + ',' +
-                        str(flags['C']) + ',' + str(data['C']) + ',' + str(sendrate['C']) + ',' + str(rate['C']) + ',' +
-                        str(flags['F']) + ',' + str(data['F']) + ',' + str(sendrate['F']) + ',' + str(rate['F']) + ',' +
-                        str(flags['D']) + ',' + str(data['D']) + ',' + str(sendrate['D']) + ',' + str(rate['D']) + ',' +
-                        str(flags['H']) + ',' + str(data['H']) + ',' + str(sendrate['H']) + ',' + str(rate['H']) + ',' +
+                        str(flags[right_thigh]) + ',' + str(data[right_thigh]) + ',' + str(sendrate[right_thigh]) + ',' + str(rate[right_thigh]) + ',' +
+                        str(flags[right_shank]) + ',' + str(data[right_shank]) + ',' + str(sendrate[right_shank]) + ',' + str(rate[right_shank]) + ',' +
+                        str(flags[back]) + ',' + str(data[back]) + ',' + str(sendrate[back]) + ',' + str(rate[back]) + ',' +
+                        str(flags[right_bicep]) + ',' + str(data[right_bicep]) + ',' + str(sendrate[right_bicep]) + ',' + str(rate[right_bicep]) + ',' +
                         str(writeRate) + ',' + str(looprate) + ',' + str(timeWrite) + '\n')
 
             file1.write(str(writes) + ',' +
-                        str(d['F'][gravaccx]) + ',' + str(d['F'][gravaccy]) + ',' + str(d['F'][gravaccz]) + ',' +
-                        str(d['H'][gravaccx]) + ',' + str(d['H'][gravaccy]) + ',' + str(d['H'][gravaccz]) + ',' +
+                        str(d[right_shank][gravaccx]) + ',' + str(d[right_shank][gravaccy]) + ',' + str(d[right_shank][gravaccz]) + ',' +
+                        str(d[right_bicep][gravaccx]) + ',' + str(d[right_bicep][gravaccy]) + ',' + str(d[right_bicep][gravaccz]) + ',' +
                         str(raRoll) + ',' + str(raPitch) + ',' + str(raYaw) + ',' + str(reach) + ',' +
-                        str(d['C'][gravaccx]) + ',' + str(d['C'][gravaccy]) + ',' + str(d['C'][gravaccz]) + ',' +
-                        str(d['F'][gravaccx]) + ',' + str(d['F'][gravaccy]) + ',' + str(d['F'][gravaccz]) + ',' +
+                        str(d[right_thigh][gravaccx]) + ',' + str(d[right_thigh][gravaccy]) + ',' + str(d[right_thigh][gravaccz]) + ',' +
+                        str(d[right_shank][gravaccx]) + ',' + str(d[right_shank][gravaccy]) + ',' + str(d[right_shank][gravaccz]) + ',' +
                         str(rlRoll) + ',' + str(rlPitch) + ',' + str(rlYaw) + ',' + str(step) + ',' +
-                        str(d['D'][gravaccx]) + ',' + str(d['D'][gravaccy]) + ',' + str(d['D'][gravaccz]) + ',' +
-                        str(d['D'][calcRoll]) + ',' + str(d['D'][calcPitch]) + ',' + str(d['D'][calcYaw]) + ',' +
+                        str(d[back][gravaccx]) + ',' + str(d[back][gravaccy]) + ',' + str(d[back][gravaccz]) + ',' +
+                        str(d[back][calcRoll]) + ',' + str(d[back][calcPitch]) + ',' + str(d[back][calcYaw]) + ',' +
                         str(forward) + ',' + str(side) + ',' + '\n')
